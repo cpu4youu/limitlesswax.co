7e4edd86ccc5c88bdc6e86d8b257a3df44e2e8ee
Isaiah <isaiahbrandtsims@gmail.com>
2022-02-14 12:14:15 -0800

    Update to pull price from blockchain

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -64,7 +64,7 @@ const Account = ({ ual }) => {
     const multiplier = 1;
     var realCost = 0.01;
     try {
-      const table = await ual.api.rpc.get_table_rows({
+      const table = await ual.rpc.get_table_rows({
         json: true, // Get the response as json
         code: "limitlesswax", // Contract that we target
         scope: "limitlesswax", // Account that owns the data
